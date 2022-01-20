b83863d9c0040be486cb0f7998f47ed580c049cf
jakubda1 <jakubec.dave@gmail.com>
2022-01-20 19:28:50 +0100

    deployment preparation pt9

--- app/serializers.py
+++ app/serializers.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import update_last_login
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.settings import api_settings
 
 from .models import *
 
@@ -18,23 +16,6 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = "__all__"
-
-
-class LoginSerializer(TokenObtainPairSerializer):
-
-    def validate(self, attrs):
-        data = super().validate(attrs)
-
-        refresh = self.get_token(self.user)
-
-        data['user'] = UserSerializer(self.user).data
-        data['refresh'] = str(refresh)
-        data['access'] = str(refresh.access_token)
-
-        if api_settings.UPDATE_LAST_LOGIN:
-            update_last_login(None, self.user)
-
-        return data
 
 
 class RegisterSerializer(UserSerializer):
